3b14c84f127e1e7a0e2c4e799bae9349ce8368f0
Freddson <mikipolu@gmail.com>
2021-04-03 00:18:30 +0200

    Added writefile to fix later

--- commands/install.js
+++ commands/install.js
@@ -6,16 +6,13 @@ function download() {
 fs.readFile('.boxcache', function(err, data) {
     const getdata = data;
     const package = getdata.toString()
+    const newjson = undefined;
         //actual download phase, first set a link to fetch info.json from
         const infojsonlink = "https://raw.githubusercontent.com/Freddson/box-packages/main/" + package + "/info.json"
         console.log(infojsonlink)
-        fetch(infojsonlink)
-        .then(res => res.json())
-        .then(json => console.log(json));
-    });
-
-    fs.unlink('.boxcache', function (err) {
-        if (err) throw err;
+        fetch(infojsonlink).then(res => res.json()).then(json => newjson => fs.writeFile('.boxjsoncache', newjson, function (err) {
+            if (err) throw err;
+          }));
     });
 }
 
